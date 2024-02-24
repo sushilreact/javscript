ce408a135263b2240a4f458c8cb63b925b5b5afb
sushilreact <sushilpal816@gmail.com>
2024-02-24 12:51:01 +0530

    stringmenthods

---
+++ string.js
@@ -0,0 +1,18 @@
+// const valName= "Sushil Pal My  Name"
+// const newval= valName.toLocaleLowerCase()
+// console.log(newval.replace("pal", "kumar"))
+
+// const incVal= "I am live in Germany" 
+// console.log(incVal.includes("of"))
+
+// const trimVal = "  sdsadad Sushil dfdsfdsf ddsd dsad"
+// console.log(trimVal.trim());
+
+// const sliceVal = "Sushil Pal Name I am live in punjab state at ludhiana city"
+// const newcvd= sliceVal.slice(-8, 11)
+// console.log(newcvd);
+
+
+const splitVal= "Sushil Pal Name I am live in punjab state at ludhiana city"
+const convertArray = splitVal.split(' ')
+console.table(convertArray)
