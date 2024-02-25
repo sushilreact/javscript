dc19934d78dfeb6fa322719af7b5640aa637377b
sushilreact <sushilpal816@gmail.com>
2024-02-25 21:01:48 +0530

    dates

---
+++ dates.js
@@ -0,0 +1,15 @@
+const myDate= new Date()
+const myCreatedDate = new Date(2023,0,21)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(typeof myDate);
+
+// console.log(myCreatedDate.toDateString())
+
+// const createTimeStamp =  Date.now()
+
+// console.log(Math.floor(createTimeStamp/1000))
+
+console.log(myDate.getDay())
+
