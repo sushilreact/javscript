1827072fe6a7cb4c6ecd296eb53fb230167cc6c4
sushilreact <sushilpal816@gmail.com>
2024-02-25 20:05:55 +0530

    Number

---
+++ number.js
@@ -0,0 +1,13 @@
+const valNumber= [2,5,7,2,8,3,7,4,4]
+
+
+// console.log(Math.ceil(valNumber))
+// console.log(Math.floor(valNumber))
+// console.log(Math.min(2,3,4,5,6,7,8))
+
+// console.log(Math.floor(Math.random()*10) + 10)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (min-max+1))+min)
