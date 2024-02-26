c426920ed4e90c51787a00af9043b2476dd7f22a
sushilreact <sushilpal816@gmail.com>
2024-02-26 16:08:55 +0530

    array

---
+++ array.js
@@ -0,0 +1,37 @@
+// const myArray = [34,45,6,77,3,2,6]
+// //console.log(myArray.unshift(96));
+// //console.log(myArray.shift());
+// //console.log(myArray.pop());
+// //console.log(myArray.includes(450));
+// //console.log(myArray.indexOf(77));
+// console.table(myArray.splice(1,4));
+// console.table("Originnal Array ", myArray)
+
+
+const bollywoodHeros = ["salman", "sharukh","Amir"]
+const southHeros= ["rajanikant", "rockey", "thala"]
+// bollywoodHeros.push(southHeros)
+// console.table(bollywoodHeros.length);
+
+// const newArrayMerge= bollywoodHeros.concat(southHeros)
+// console.table(newArrayMerge)
+
+// const newSpread= [...bollywoodHeros,...southHeros]
+// console.table(newSpread)
+
+// const nestedArrayIntoConvertSingle= [2,3,4,6,[56,34,24,64,[5,7,4,65,89,5],[65,34,24,13,64,75]]]
+// const newArr= nestedArrayIntoConvertSingle.flat(Infinity)
+// console.table(newArr)
+// console.log(Array.isArray(newArr))
+
+// const hello ="Helllo Sushil pal"
+
+// console.log(Array.from(hello))
+
+let a=23
+let b=45
+let c=67
+
+const newarr= Array.of(a,b,c)
+console.table(newarr)
+
