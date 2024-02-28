4b5ac4878e04b2cc6529b159e52c4f28024a14e7
sushilreact <sushilpal816@gmail.com>
2024-02-28 19:59:31 +0530

    switch

---
+++ controlflow/switch.js
@@ -0,0 +1,13 @@
+// const username = "mohit"
+// switch (username) {
+//     case "mohit":
+//         console.log("wrong name");
+//         break;
+//         case "sushil":
+//             console.log("right  name");
+//             break;
+
+//     default:
+//         console.log("Nothing Found");
+//         break;
+// }
---
+++ ifif.js
@@ -0,0 +1,6 @@
+// (function Invoked(){
+//     console.log("Hello I am Inovked")
+//    })()
+   (()=>{
+    console.log("hello Sushil");
+   })()
