f60c1dadfc41ed73cb97be562d8154f313ab8769
sushilreact <sushilpal816@gmail.com>
2024-03-02 19:58:02 +0530

    revision

---
+++ controlflow/map.js
@@ -0,0 +1,35 @@
+// const myNumber = []
+// const mynumArray = [1,2,3,4,5,6,7,8,9,10]
+
+// const newnum = mynumArray.forEach((itme)=>{
+//      myNumber.push(itme+10)
+// })
+
+// console.log(myNumber)
+
+// const myNumber= [34,45,64,75,86]
+// const newArra = myNumber.filter((item )=>{
+//      return item >64
+// })
+
+// console.log(newArra)
+
+
+const arr = [56,34,66,86,96]
+
+const newArray = arr.map((item)=>{
+     return item
+})
+
+console.log(newArray.sort())
+
+// const newArray= arr.map((item)=>{
+//      return item+1
+// })
+// .map((item)=>{
+//      return item*2
+// })
+// .filter((item)=>{
+//      return item<=100
+// })
+// console.log(newArray);
